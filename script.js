626d7cb1d781f6a6ffff8b638eb2898a3bd89663
SurjeetMandal <surjeetmandal698@gmail.com>
2024-04-19 20:36:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
+
 document.addEventListener("DOMContentLoaded", function() {
-
   var body = document.querySelector("body");
-
 
   if (body.hasChildNodes()) {
     body.innerHTML = "";
   }
 
-  var  DomLoad= document.createTextNode("DOM load success");
-  body.appendChild(DomLoad);
+  var textNode = document.createTextNode("DOM load success");
+
+  body.appendChild(textNode);
 });
 
