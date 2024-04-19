da54c33b68cc6084c73428a3d0bd0a546504b63a
SurjeetMandal <surjeetmandal698@gmail.com>
2024-04-19 20:17:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-document.addEventListener("DOMContentLoaded", function() {
+
 	  
 	var para = document.createElement("p");
     para.textContent = "DOM load success";
     document.body.appendChild(para);
-});
+
 
