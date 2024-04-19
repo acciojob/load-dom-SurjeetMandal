e8c48524c8ee1d7d90ad5e9cd037a782a3ee7a17
SurjeetMandal <surjeetmandal698@gmail.com>
2024-04-19 20:19:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
-
-	  
-	var para = document.createElement("p");
+document.addEventListener("DOMContentLoaded", function() {
+  
+  if (!document.body.hasChildNodes()) {
+    
+    var para = document.createElement("p");
     para.textContent = "DOM load success";
     document.body.appendChild(para);
-
+  } else {
+    console.error("Body is not empty");
+  }
+});
 
