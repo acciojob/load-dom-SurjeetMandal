1db6ea54d00baca217216c71de1626c020f31f71
SurjeetMandal <surjeetmandal698@gmail.com>
2024-04-19 20:05:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", function() {
   let para = document.createElement('p');
   para.innerHTML = `DOM load success`;
-  document.body.appendChild(para); // Append the paragraph to the body or another element in the DOM
+  document.body.appendChild(para); 
 });
